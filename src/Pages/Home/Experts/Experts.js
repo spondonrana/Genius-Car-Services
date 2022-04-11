f9ebc821584613155b616895adddae6a510930bf
Masud Rana <54145451+spondonrana@users.noreply.github.com>
2022-04-12 01:55:46 +0600

    banner add

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -20,7 +20,7 @@ const experts = [
 const Experts = () => {
   return (
     <div className="container">
-      <h2 className="text-primary text-center">Our Experts Man</h2>
+      <h2 className="text-primary text-center mt-5">Our Experts Man</h2>
       <div className="row">
         {experts.map((expert) => (
           <Expert key={expert.id} expert={expert}></Expert>
